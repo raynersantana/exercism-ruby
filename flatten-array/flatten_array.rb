07a2eed4a90894ff82f102c076b0683aa66134c1
Rayner <raynersan@hotmail.com>
2018-09-25 19:18:17 +0000

    Some challenges

---
+++ flatten-array/flatten_array.rb
@@ -0,0 +1,13 @@
+class FlattenArray
+  def self.flatten(array)
+    array.reject(&:nil?).each_with_object([]) do |item, result|
+      if item.is_a?(Array)
+        FlattenArray.flatten(item).each do |item|
+          result << item
+        end
+      else
+        result << item
+      end
+    end
+  end
+end
---
+++ gigasecond/gigasecond.rb
@@ -0,0 +1,5 @@
+class Gigasecond
+    def self.from(time_in)
+        time_in + 10 ** 9
+    end
+end
